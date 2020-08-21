aad35bfdfaa538a90ba4ee491c9a904120ab6004
“sss-o2” <your@example.com>
2020-08-21 16:58:35 +0900

    gitignore vue bootstrap jquery popper.js追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
+import Rails from '@rails/ujs';
+Rails.start();
+
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require('jquery')
 require("channels")
+require("bootstrap")
+
+import 'bootstrap'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
+const webpack = require('webpack')
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        Rails: ['@rails/ujs']
+    })
+)
 
 module.exports = environment
