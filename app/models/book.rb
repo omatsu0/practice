7319218b70eaa39d4b77ed8b5943bf78cdb29419
“sss-o2” <your@example.com>
2020-08-24 23:30:26 +0900

    validation追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,21 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name,length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validate do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+  before_validation do
+    self.name = self.name.gsub(/Cat/) do |matched|
+      "lovely #{matched}"
+    end
+  end
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
 end
